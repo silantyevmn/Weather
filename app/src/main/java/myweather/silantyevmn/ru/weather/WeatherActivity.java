5f39bb033ecd09d473e08ac89db9406e8b0fadde
silantyevmn <5992033@mail.ru>
2018-04-12 14:42:41 +0300

    добавил Акшен Бар убрал с формы 2 чекета добавил их в акшен бар

--- app/src/main/java/myweather/silantyevmn/ru/weather/WeatherActivity.java
+++ app/src/main/java/myweather/silantyevmn/ru/weather/WeatherActivity.java
@@ -5,29 +5,33 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Switch;
 
 public class WeatherActivity extends AppCompatActivity implements CityListFragment.CityListListener {
     private WeatherPreferencer preferencer;
-    private Switch switchPressure,switchWind;
+    private Switch switchPressure, switchWind;
+    private MenuItem itemPressure, itemWind;
+    //private ListView listView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        if(CityEmitter.getCities()==null){
+        if (CityEmitter.getCities() == null) {
             CityEmitter.initNewCityParam(WeatherActivity.this);
         }
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        preferencer=new WeatherPreferencer(this);
-        switchWind=findViewById(R.id.switchWind);
-        switchPressure=findViewById(R.id.switchPressure);
+        preferencer = new WeatherPreferencer(this);
+        //switchWind=findViewById(R.id.switchWind);
+        //switchPressure=findViewById(R.id.switchPressure);
         if (savedInstanceState == null) {
             //загрузка
             load();
         }
     }
+
 
     @Override
     public void onListItemClick(int positionID) {
@@ -38,13 +42,13 @@ public class WeatherActivity extends AppCompatActivity implements CityListFragme
         transaction.replace(R.id.fragment_container, detailFragment);
         transaction.commit();*/
 
-        View container=findViewById(R.id.fragment_city_detail);
-        CityEmitter.isWind=switchWind.isChecked();
-        CityEmitter.isPressure=switchPressure.isChecked();
+        View container = findViewById(R.id.fragment_city_detail);
+        CityEmitter.isWind = itemWind.isChecked();
+        CityEmitter.isPressure = itemPressure.isChecked();
 
-        if(container==null){
-            Intent intent=new Intent(this,CityDetailActivity.class);
-            intent.putExtra(CityDetailActivity.EXTRA_POSITION_ID,positionID);
+        if (container == null) {
+            Intent intent = new Intent(this, CityDetailActivity.class);
+            intent.putExtra(CityDetailActivity.EXTRA_POSITION_ID, positionID);
             startActivity(intent);
         } else {
             // Инстантиируем Fragment
@@ -60,7 +64,33 @@ public class WeatherActivity extends AppCompatActivity implements CityListFragme
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
+        itemPressure = menu.findItem(R.id.item_pressure);
+        itemWind = menu.findItem(R.id.item_wind);
+        load();
         return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case R.id.item_pressure: {
+                itemClickChecked(item, item.isChecked());
+                return false;
+            }
+            case R.id.item_wind: {
+                itemClickChecked(item, item.isChecked());
+                return false;
+            }
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
+
+    private void itemClickChecked(MenuItem item, boolean isChecked) {
+        item.setChecked(!isChecked);
+        save();
     }
 
     @Override
@@ -70,11 +100,16 @@ public class WeatherActivity extends AppCompatActivity implements CityListFragme
     }
 
     private void save() {
-        preferencer.save(switchWind.isChecked(),switchPressure.isChecked());
+        //preferencer.save(switchWind.isChecked(),switchPressure.isChecked());
+        if (itemWind != null & itemPressure != null) {
+            preferencer.save(itemWind.isChecked(), itemPressure.isChecked());
+        }
     }
 
     private void load() {
-        switchWind.setChecked(preferencer.getSwitchWind());
-        switchPressure.setChecked(preferencer.getSwitchPressure());
+        if (itemWind != null) itemWind.setChecked(preferencer.getIsWind());
+        if (itemPressure != null) itemPressure.setChecked(preferencer.getIsPressure());
+        //switchWind.setChecked(preferencer.getIsWind());
+        //switchPressure.setChecked(preferencer.getIsPressure());
     }
 }
